6094e83e371356bc009793512e98b32c8e8f877b
Mazharrais <mazharmajo72@gmail.com>
2024-07-26 12:32:58 +0500

    add code

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 
+let toastBox = document.getElementById("toastBox");
 
 
 
 function showToast(){
 
-    
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = "success";
+    toastBox.appendChild(toast);
+
 }
