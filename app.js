a39dd6aea983626851ff25bc88abd70d97283a03
Mazharrais <mazharmajo72@gmail.com>
2024-07-26 15:53:56 +0500

    add code

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 
 let toastBox = document.getElementById("toastBox");
+let successMsg = "successfully submitted...";
+let errorMsg = "Please fix the error...";
+let invalidMsg = "Invalid Input please check again...";
 
 
 
-function showToast(){
+function showToast(msg){
 
     let toast = document.createElement('div');
     toast.classList.add('toast');
-    toast.innerHTML = "success";
+    toast.innerHTML = msg;
     toastBox.appendChild(toast);
 
 }
